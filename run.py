b80358c7db8b6d3c2d85eb68ab85ba47ffe93302
Jackson Jarboe <122476654+JacksonJ-KC@users.noreply.github.com>
2025-04-24 08:33:55 -0400

    organize config

--- run.py
+++ run.py
@@ -2,18 +2,38 @@ from pathlib import Path
 
 from rpd_tester.perform_comparison import results_data, run_comparison_for_all_tests
 
-
-CONFIG_DATA = {
-    "generation_software_name": "Karpman Consulting RPD Generator",
-    "generation_software_version": "1.0.0",
-    "modeling_software_name": "eQUEST/DOE2.3",
-    "modeling_software_version": "3.65.7175",
-    "schema_version": "0.1.4",
-    "ruleset_name": "ASHRAE Standard 90.1-2019, Performance Rating Method",
-    "ruleset_checking_specification_name": "ASHRAE Standard 90.1-2019, Performance Rating Method",
-}
+# ---------- SET THE VALUES FOR ALL CONFIGURATION VARIABLES ----------
+GENERATION_SOFTWARE_NAME = ""
+GENERATION_SOFTWARE_VERSION = ""
+MODELING_SOFTWARE_NAME = ""
+MODELING_SOFTWARE_VERSION = ""
+SCHEMA_VERSION = "0.0.36"
+RULESET_NAME = "ASHRAE Standard 90.1-2019, Performance Rating Method"
+RULESET_CHECKING_SPECIFICATION_NAME = "ASHRAE Standard 90.1-2019, Performance Rating Method"
+# --------------------------------------------------------------------
 
 if __name__ == "__main__":
+    config_vars = [
+        GENERATION_SOFTWARE_NAME,
+        GENERATION_SOFTWARE_VERSION,
+        MODELING_SOFTWARE_NAME,
+        MODELING_SOFTWARE_VERSION,
+        SCHEMA_VERSION,
+        RULESET_NAME,
+        RULESET_CHECKING_SPECIFICATION_NAME,
+    ]
+    if any(not var for var in config_vars):
+        raise ValueError("All configuration variables must be set in 'run.py'.")
+
+    CONFIG_DATA = {
+        "generation_software_name": GENERATION_SOFTWARE_NAME,
+        "generation_software_version": GENERATION_SOFTWARE_VERSION,
+        "modeling_software_name": MODELING_SOFTWARE_NAME,
+        "modeling_software_version": MODELING_SOFTWARE_VERSION,
+        "schema_version": SCHEMA_VERSION,
+        "ruleset_name": RULESET_NAME,
+        "ruleset_checking_specification_name": RULESET_CHECKING_SPECIFICATION_NAME,
+    }
     test_directory = Path(__file__).resolve().parent / "bem_test_files"
     results_data.update(CONFIG_DATA)
     run_comparison_for_all_tests(test_directory)
